6703760aae6811051c790b344a39d5ad529208f5
Jeremyea3001 <151014662+Jeremyea3001@users.noreply.github.com>
2024-06-11 13:23:30 +0200

    Update navbar.inc.php

--- navbar.inc.php
+++ navbar.inc.php
@@ -1,6 +1,6 @@
 <?php
-if (! isset($lang)) {
-  $lang = "fr";
+if (!isset($lang)) {
+    $lang = "fr";
 }
 
 $adresse = $_SERVER['PHP_SELF'];
@@ -8,6 +8,15 @@ $composants_adresse = explode('/', $adresse);
 $fichier_avec_ext = end($composants_adresse);
 $fichier_sep = explode('.', $fichier_avec_ext);
 $nom_fichier = $fichier_sep[0];
+
+$langs = [
+    'fr' => ['Rechercher...', 'Participer', 'Parcourir', 'Découvrir', 'FR', 'https://i.imgur.com/moe4dvF.png'],
+    'en' => ['Research...', 'Participate', 'Route', 'Discover', 'EN', 'https://i.imgur.com/ZUd06Ts.png'],
+    'es' => ['Buscar...', 'Participar', 'Explorar', 'Descubrir', 'ES', 'https://i.imgur.com/6MrXRwG.png']
+];
+
+$current_lang = $langs[$lang];
+$other_langs = array_diff_key($langs, [$lang => '']);
 
 echo "
 <!-- Barre de navigation -->
@@ -19,139 +28,46 @@ echo "
         <img id='hambmenu' src='https://i.imgur.com/MCTSRpB.png'>
       </button>
       <!-- Logo MCN -->
-      <a class='logo' href='index.php?lang=$lang'><img id='LogoMCN' src='https://i.imgur.com/FYHl6nN.png'></a>
+      <a class='logo' href='index.php?lang=" . htmlspecialchars($lang) . "'><img id='LogoMCN' src='https://i.imgur.com/FYHl6nN.png'></a>
     </div>
         
     <!-- Barre de recherche -->
-    <form method='post' action='?.php?lang=$lang'>
-";
-
-switch($lang) {
-  case 'fr':
-    echo "<input class='recherche' type='text' placeholder='Rechercher...'>";
-    break;
-  case 'en':
-    echo "<input class='recherche' type='text' placeholder='Research...'>";
-    break;
-  case 'es':
-    echo "<input class='recherche' type='text' placeholder='Buscar...'>";
-    break;
+    <div id='droite'>
+      <form method='post' action='" . htmlspecialchars($nom_fichier) . ".php?lang=" . htmlspecialchars($lang) . "'>
+        <input class='recherche' type='text' placeholder='" . htmlspecialchars($current_lang[0]) . "'>
+        <input class='loupe' type='submit' value=''>
+      </form>
+      <!-- Menu des langues -->    
+      <div id='container'>
+        <button class='menul' onClick='myFunctionlang(\"langues\")'>
+          <img src='" . htmlspecialchars($current_lang[5]) . "' class='langue'/>&nbsp;" . htmlspecialchars($current_lang[4]) . "
+        </button>
+        <div id='langues' style='display: none;'>";
+foreach ($other_langs as $key => $value) {
+    echo "
+          <a href='" . htmlspecialchars($nom_fichier) . ".php?lang=" . htmlspecialchars($key) . "'>
+            <div class='sousl'>
+              <img src='" . htmlspecialchars($value[5]) . "' class='langue'/>&nbsp;" . htmlspecialchars($value[4]) . "
+            </div>
+          </a>";
 }
-
 echo "
-      <input class='loupe' type='submit' value=''>
-    </form>
-    <!-- Menu des langues -->    
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Reste du menu hamburger -->
+  <div id='menu' style='display: none;'>
+    <div class='sous'>
+      <a href='participer.php?lang=" . htmlspecialchars($lang) . "'>" . htmlspecialchars($current_lang[1]) . "</a>
+    </div>
+    <div class='sous'>
+      <a href='parcourir.php?lang=" . htmlspecialchars($lang) . "'>" . htmlspecialchars($current_lang[2]) . "</a>
+    </div>
+    <div class='sous'>
+      <a href='decouvrir.php?lang=" . htmlspecialchars($lang) . "'>" . htmlspecialchars($current_lang[3]) . "</a>
+    </div>
+  </div>
+</div>
 ";
-
-switch ($lang) {
-  case 'fr':
-    echo "
-    <button class='menul' onClick='myFunction(\"langues\")'>
-      <img src='https://i.imgur.com/moe4dvF.png' class='langue'/>&nbsp;FR
-    </button>
-    <div id='langues' style='display: none;'>
-      <a href='$nom_fichier.php?lang=en'>
-        <div class='sousl'>
-          <img src='https://i.imgur.com/ZUd06Ts.png' class='langue'/>&nbsp;EN
-        </div>
-      </a>
-      <a href='$nom_fichier.php?lang=es'>
-        <div class='sousl'>
-          <img src='https://i.imgur.com/6MrXRwG.png' class='langue'/>&nbsp;ES
-        </div>
-      </a>
-    </div>
-  </div>";
-    break;
-  case 'en':
-    echo "
-    <button class='menul' onClick='myFunction(\"langues\")'>
-      <img src='https://i.imgur.com/ZUd06Ts.png' class='langue'/>&nbsp;EN
-    </button>
-    <div id='langues' style='display: none;'>
-      <a href='$nom_fichier.php?lang=fr'>
-        <div class='sousl'>
-          <img src='https://i.imgur.com/moe4dvF.png' class='langue'/>&nbsp;FR
-        </div>
-      </a>
-      <a href='$nom_fichier.php?lang=es'>
-        <div class='sousl'>
-          <img src='https://i.imgur.com/6MrXRwG.png' class='langue'/>&nbsp;ES
-        </div>
-      </a>
-    </div>
-  </div>";
-    break;
-  case 'es':
-    echo "
-    <button class='menul' onClick='myFunction(\"langues\")'>
-      <img src='https://i.imgur.com/6MrXRwG.png' class='langue'/>&nbsp;ES
-    </button>
-    <div id='langues' style='display: none;'>
-      <a href='$nom_fichier.php?lang=fr'>
-        <div class='sousl'>
-          <img src='https://i.imgur.com/moe4dvF.png' class='langue'/>&nbsp;FR
-        </div>
-      </a>
-      <a href='$nom_fichier.php?lang=en'>
-        <div class='sousl'>
-          <img src='https://i.imgur.com/ZUd06Ts.png' class='langue'/>&nbsp;EN
-        </div>
-      </a>
-    </div>
-  </div>";
-    break;
-}
-
-switch ($lang) {
-  case 'fr':
-    echo "
-    <!-- Reste du menu hamburger -->
-    <div id='menu' style='display: none;'>
-      <div class='sous'>
-        <a href='participer.php?lang=$lang'>Participer</a>
-      </div>
-      <div class='sous'>
-        <a href='parcourir.php?lang=$lang'>Parcourir</a>
-      </div>
-      <div class='sous'>
-        <a href='decouvrir.php?lang=$lang'>Découvrir</a>
-      </div>
-    </div>
-  </div>";
-    break;
-  case 'en':
-    echo "
-    <!-- Reste du menu hamburger -->
-    <div id='menu' style='display: none;'>
-      <div class='sous'>
-        <a href='participer.php?lang=$lang'>Participate</a>
-      </div>
-      <div class='sous'>
-        <a href='parcourir.php?lang=$lang'>Browse</a>
-      </div>
-      <div class='sous'>
-        <a href='decouvrir.php?lang=$lang'>Discover</a>
-      </div>
-    </div>
-  </div>";
-    break;
-  case 'es':
-    echo "
-    <!-- Reste du menu hamburger -->
-    <div id='menu' style='display: none;'>
-      <div class='sous'>
-        <a href='participer.php?lang=$lang'>Participar</a>
-      </div>
-      <div class='sous'>
-        <a href='parcourir.php?lang=$lang'>Explorar</a>
-      </div>
-      <div class='sous'>
-        <a href='decouvrir.php?lang=$lang'>Descubrir</a>
-      </div>
-    </div>
-  </div>";
-    break;
-}
 ?>
