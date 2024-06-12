9425ea97d36b0da52723135ab0c16c269a40851e
Jeremyea3001 <151014662+Jeremyea3001@users.noreply.github.com>
2024-06-12 09:55:06 +0200

    Update checklang.inc.php

--- checklang.inc.php
+++ checklang.inc.php
@@ -1,5 +1,5 @@
 <?php
-if (! isset($_REQUEST["lang"])) {
+if (! (isset($_REQUEST["lang"]) || in_array($lang, ["fr", "en", "es"]))) {
     $adresse = $_SERVER['PHP_SELF'];
     header("Location: $adresse?lang=fr");
 } else {
