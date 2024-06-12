12a51a5b0a10e1fa592e71d1322b5cf8daa85e04
Jeremyea3001 <151014662+Jeremyea3001@users.noreply.github.com>
2024-06-12 23:21:57 +0200

    Update checklang.inc.php
    petit fix pour 'lang='

--- checklang.inc.php
+++ checklang.inc.php
@@ -1,8 +1,14 @@
 <?php
-if (! (isset($_REQUEST["lang"]) || in_array($lang, ["fr", "en", "es"]))) {
+if (isset($_REQUEST["lang"]) ) {
+    $lang = $_REQUEST['lang'];
+    if (! in_array($lang, ["fr", "en", "es"])) {
+        $adresse = $_SERVER['PHP_SELF'];
+        header("Location: $adresse?lang=fr");
+        exit(0);
+    }
+} else {
     $adresse = $_SERVER['PHP_SELF'];
     header("Location: $adresse?lang=fr");
-} else {
-    $lang = $_REQUEST['lang'];
+    exit(0);
 }
 ?>
