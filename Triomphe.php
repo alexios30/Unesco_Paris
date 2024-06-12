e54073f9ef5add37c6d9566fcd1ac25d01afef02
alexios30 <147252629+alexios30@users.noreply.github.com>
2024-06-12 10:02:21 +0200

    ajout d'un case avec l'anglais

--- Triomphe.php
+++ Triomphe.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
   </head>
   <body>
+    
   <?php
     include("checklang.inc.php");
     ?>
@@ -15,6 +16,7 @@
     
 <br/>
 <br/>
+
   <div class="about2">
     <span class="gros">Arc de Triomphe</span>
   </div>
@@ -26,98 +28,182 @@
     <br>
     <div class="Champs-Info">
     </div>
+    <?php
+     switch ($lang) {
+      case "fr":
 
-    <div class="center-text0">
-        <p>L'Arc de Triomphe est un monument emblématique de Paris.</p>
+    echo '<div class="center-text0">
+        <p>L\'Arc de Triomphe est un monument emblématique de Paris.</p>
     </div>
     <div class="center-text0">
-      <p> Construit pour célébrer les victoires militaires de Napoléon Bonaparte, il trône au bout de l'avenue des Champs-Élysées.</p>
+      <p>Construit pour célébrer les victoires militaires de Napoléon Bonaparte, il trône au bout de l\'avenue des Champs-Élysées.</p>
+    </div>
+    <div class="center-text0">
+      <p>Sa conception imposante et ses bas-reliefs commémoratifs en font un symbole puissant de la gloire et de la grandeur nationales françaises.</p>
+    </div>
+    <br>
+    <div class="titre-gauche">
+      <span class="gros2">L\'histoire de L\'Arc de Triomphe</span>
+    </div>
+    <div class="texte-au-dessus-de-ligne">
+      <p>Le projet Napoléonien</p>
+      <div class="ligne3"></div>
+    </div>
+    <br>
+    <div class="Champs-Info2"></div>
+    <div class="description">
+      <p>Photo de L\'Arc de triomphe de nos jours</p>
+    </div>
+    <div class="center-text">
+      <p>Le projet Napoléonien de construction de l\'Arc de Triomphe trouve son origine avec les architectes Jean-François Thérèse Chalgrin et Jean-Arnaud Raymond, qui ont choisi la Place de l\'Étoile comme site emblématique. Inspiré par des références antiques, notamment l\'Arc de Titus à Rome, le design de l\'arc opte pour une ouverture simple, symbolisant également une porte de la ville.</p>
+    </div>
+    <div class="center-text">
+      <p>Le processus de construction a connu des défis, notamment lors du mariage de Napoléon 1er et Marie-Louise d\'Autriche en 1810. Alors que l\'itinéraire impérial prévoyait de passer par la Place de l\'Étoile, le chantier de l\'Arc de Triomphe n\'était pas terminé. Pour pallier cela, une maquette grandeur nature, construite en charpentes et toiles peintes, a été érigée de manière éphémère. Cette construction temporaire, coûtant 511 000 francs, a permis à Chalgrin d\'apporter des ajustements aux plans originaux, influençant notamment les ressauts, la suppression des socles, et le choix de l\'ornementation des façades.</p>
+    </div>
+    <div class="center-text">
+      <p>Cependant, en janvier 1811, Chalgrin décède avec les piliers de l\'Arc de Triomphe à une hauteur modeste. Son élève, Louis-Robert Goust, prend la relève du projet. Malgré les défis, ces événements ont façonné la construction de l\'Arc de Triomphe, en révélant l\'ingéniosité des architectes et en illustrant la persévérance face aux obstacles.</p>
+    </div>
+    <div class="titre-gauche"></div>
+    <div class="sousTitre">
+      <h2>Un chantier suspendu sous Louis XVIII</h2>
+    </div>
+    <div class="center-text">
+      <p></p>
+    </div>
+    <div class="center-text">
+      <p>Après la chute de Napoléon et l\'avènement de Louis XVIII, le chantier de l\'Arc de Triomphe est interrompu. En 1814, l\'architecte Bernard Poyet propose même de démolir les piliers existants, mais cette suggestion est rejetée par Louis XVIII, bien qu\'aucune intention de reprendre la construction ne soit clairement exprimée. Les monarchistes critiquent ce projet commandé par Napoléon, et diverses propositions alternatives entre 1814 et 1823 ne suscitent aucun intérêt royal.</p>
+    </div>
+    <div class="center-text">
+      <p>Le 9 octobre 1823, Louis XVIII décide de reprendre immédiatement et de terminer l\'Arc de Triomphe, mais avec une nouvelle dédicace célébrant l\'Armée des Pyrénées. Les travaux reprennent progressivement, sous la direction de Louis-Robert Goust, désormais associé à l\'architecte Jean-Nicolas Huyot. Ensemble, ils retravaillent les plans de Chalgrin, ajoutant notamment des colonnes en façade. Charles X succède à Louis XVIII en 1824 et poursuit les travaux selon les souhaits de son prédécesseur.</p>
+    </div>
+    <div class="center-text">
+      <p>En 1825, Huyot est révoqué pour sa proposition de colonnes en façade, mais il est réintégré en 1826. Charles X insiste pour que les plans de Chalgrin soient respectés. En 1828, une commission d\'architectes définit les ornements à réaliser, incluant des caissons avec rosaces et des hauts reliefs sculptés sur les façades. La pierre de Chérence est choisie pour les sculptures de l\'entablement. En 1829, le monument atteint l\'architrave de l\'entablement, et une plaque de marbre avec la nouvelle dédicace à l\'Armée des Pyrénées est installée. En 1830, Goust se retire, laissant Huyot seul sur le chantier. Ce dernier propose un projet ornant la corniche supérieure de 36 statues symbolisant les grandes villes de France.</p>
+    </div>
+    <div class="center-text">
+      <p></p>
+    </div>
+    <div class="rectangle-orange">
+      <p>Vous souhaitez visiter Paris mais vous ne savez pas par où commencer ?</p>
+      <p>Découvrez nos parcours prédéfinis selon votre temps et vos envies !</p>
+      <div class="texterectangle">
+        <a href="Parcourir.php">Parcourez ></a>
+      </div>
+    </div>
+    <div class="texte-au-dessus-de-ligne">
+      <p>L\'inauguration de L\'Arc de triomphe</p>
+      <div class="ligne3"></div>
+    </div>
+    <div class="Champs-Info3"></div>
+    <div class="sousTitre">
+      <h2>Une inauguration discrète</h2>
+    </div>
+    <div class="center-text2">
+      <p>Le 29 juillet 1836 marque l\'inauguration de l\'Arc de Triomphe après trois décennies de travaux. Les listes de noms gravés sur les piédroits sont dévoilées lors de la cérémonie, mais la grande fête prévue est annulée par crainte d\'attentat contre le roi Louis-Philippe. Seulement onze personnes assistent à l\'événement, dont le président du Conseil des ministres Adolphe Thiers et le ministre des Finances Antoine Maurice Appolinaire Argout.</p>
+    </div>
+    <div class="center-text2">
+      <p>À la nuit tombée, une foule se rassemble devant l\'Arc de Triomphe illuminé de 700 becs de gaz. Cependant, des controverses surgissent concernant les noms inscrits sur les piliers, entraînant des protestations. En réponse, le gouvernement et l\'architecte promettent d\'étudier toutes les demandes. L\'architecte Blouet ajoute rapidement 128 noms de généraux et 172 batailles oubliés. Dans son ouvrage de 1837 intitulé "Les Voix Intérieures", Victor Hugo exprimera son chagrin de ne pas voir le nom de son père, Jospeh-Léopold-Sigisbert Hugo, nommé général par Louis XVIII en 1814, figurant sur l\'Arc.</p>
+    </div>
+    <div class="center-text2">
+      <p>Des ajouts ponctuels aux noms gravés sur l\'Arc de Triomphe se poursuivent jusqu\'en 1895, reflétant ainsi une histoire complexe et dynamique liée à ce monument emblématique de Paris.</p>
+    </div>
+    <br>
+    <br>
+    <div class="texte-au-dessus-de-ligne">
+      <div class="ligne3"></div>
+    </div>';
+
+    break;
+
+    case "en": 
+      echo '<div class="center-text0">
+      <p>The Arc de Triomphe is an iconic monument in Paris.</p>
   </div>
   <div class="center-text0">
-    <p>Sa conception imposante et ses bas-reliefs commémoratifs en font un symbole puissant de la gloire et de la grandeur nationales françaises.</p>
+    <p>Built to celebrate Napoleon Bonaparte\'s military victories, it stands at the end of the Champs-Élysées avenue.</p>
   </div>
-    <br>
+  <div class="center-text0">
+    <p>Its imposing design and commemorative bas-reliefs make it a powerful symbol of French national glory and grandeur.</p>
+  </div>
+  <br>
   <div class="titre-gauche">
-  <span class="gros2">L'histoire de L'Arc de Triomphe</span>
+    <span class="gros2">The history of the Arc de Triomphe</span>
   </div>
   <div class="texte-au-dessus-de-ligne">
-    <p>Le projet Napoléonien</p>
+    <p>The Napoleonic project</p>
     <div class="ligne3"></div>
   </div>
   <br>
-  <div class="Champs-Info2">
-  </div>
+  <div class="Champs-Info2"></div>
   <div class="description">
-  <p>Photo de L'Arc de triomphe de nos jours</p>
-  </div>
-    <div class="center-text">
-      <p> 
-        Le projet Napoléonien de construction de l'Arc de Triomphe trouve son origine avec les architectes Jean-François Thérèse Chalgrin et Jean-Arnaud Raymond, qui ont choisi la Place de l’Étoile comme site emblématique. Inspiré par des références antiques, notamment l'Arc de Titus à Rome, le design de l'arc opte pour une ouverture simple, symbolisant également une porte de la ville.</p>
+    <p>Photo of the Arc de Triomphe today</p>
   </div>
   <div class="center-text">
-      <p>Le processus de construction a connu des défis, notamment lors du mariage de Napoléon 1er et Marie-Louise d'Autriche en 1810. Alors que l'itinéraire impérial prévoyait de passer par la Place de l’Étoile, le chantier de l'Arc de Triomphe n'était pas terminé. Pour pallier cela, une maquette grandeur nature, construite en charpentes et toiles peintes, a été érigée de manière éphémère. Cette construction temporaire, coûtant 511 000 francs, a permis à Chalgrin d'apporter des ajustements aux plans originaux, influençant notamment les ressauts, la suppression des socles, et le choix de l'ornementation des façades.</p>
+    <p>The Napoleonic project to build the Arc de Triomphe originated with the architects Jean-François Thérèse Chalgrin and Jean-Arnaud Raymond, who chose the Place de l \'Étoile as the emblematic site. Inspired by ancient references, notably the Arch of Titus in Rome, the design of the arch opts for a simple opening, also symbolising a gateway to the city.</p>
   </div>
   <div class="center-text">
-    <p> Cependant, en janvier 1811, Chalgrin décède avec les piliers de l'Arc de Triomphe à une hauteur modeste. Son élève, Louis-Robert Goust, prend la relève du projet. Malgré les défis, ces événements ont façonné la construction de l'Arc de Triomphe, en révélant l'ingéniosité des architectes et en illustrant la persévérance face aux obstacles.</p>
+    <p>The construction process was not without its challenges, particularly at the time of the marriage of Napoleon 1st and Marie-Louise of Austria in 1810. Although the imperial itinerary planned to pass through the Place de l\'Étoile, the Arc de Triomphe construction site had not been completed. To compensate for this, a life-size mock-up was erected from painted canvas and framework. This temporary construction, costing 511,000 francs, allowed Chalgrin to make adjustments to the original plans, influencing in particular the projections, the removal of the plinths, and the choice of ornamentation for the façades.</p>
   </div>
-  <div class="titre-gauche">
+  <div class="center-text">
+    <p>However, in January 1811, Chalgrin died with the pillars of the Arc de Triomphe at a modest height. His pupil, Louis-Robert Goust, took over the project. Despite the challenges, these events shaped the construction of the Arc de Triomphe, revealing the ingenuity of the architects and illustrating perseverance in the face of obstacles.</p>
   </div>
+  <div class="titre-gauche"></div>
   <div class="sousTitre">
-    <h2>Un chantier suspendu sous Louis XVIII</h2>
-  </div>
-    <div class="center-text">
-      <p> </p>
+    <h2>A building site suspended under Louis XVIII</h2>
   </div>
   <div class="center-text">
-      <p> Après la chute de Napoléon et l'avènement de Louis XVIII, le chantier de l'Arc de Triomphe est interrompu. En 1814, l'architecte Bernard Poyet propose même de démolir les piliers existants, mais cette suggestion est rejetée par Louis XVIII, bien qu'aucune intention de reprendre la construction ne soit clairement exprimée. Les monarchistes critiquent ce projet commandé par Napoléon, et diverses propositions alternatives entre 1814 et 1823 ne suscitent aucun intérêt royal.</p>
+    <p></p>
   </div>
   <div class="center-text">
-    <p>Le 9 octobre 1823, Louis XVIII décide de reprendre immédiatement et de terminer l'Arc de Triomphe, mais avec une nouvelle dédicace célébrant l'Armée des Pyrénées. Les travaux reprennent progressivement, sous la direction de Louis-Robert Goust, désormais associé à l'architecte Jean-Nicolas Huyot. Ensemble, ils retravaillent les plans de Chalgrin, ajoutant notamment des colonnes en façade. Charles X succède à Louis XVIII en 1824 et poursuit les travaux selon les souhaits de son prédécesseur.</p>
+    <p>After the fall of Napoleon and the accession of Louis XVIII, work on the Arc de Triomphe was interrupted. In 1814, the architect Bernard Poyet even proposed demolishing the existing pillars, but this suggestion was rejected by Louis XVIII, although no intention to resume construction was clearly expressed. The monarchists criticised this project commissioned by Napoleon, and various alternative proposals between 1814 and 1823 failed to arouse any royal interest.</p>
   </div>
   <div class="center-text">
-    <p> En 1825, Huyot est révoqué pour sa proposition de colonnes en façade, mais il est réintégré en 1826. Charles X insiste pour que les plans de Chalgrin soient respectés. En 1828, une commission d'architectes définit les ornements à réaliser, incluant des caissons avec rosaces et des hauts reliefs sculptés sur les façades. La pierre de Chérence est choisie pour les sculptures de l'entablement. En 1829, le monument atteint l'architrave de l'entablement, et une plaque de marbre avec la nouvelle dédicace à l'Armée des Pyrénées est installée. En 1830, Goust se retire, laissant Huyot seul sur le chantier. Ce dernier propose un projet ornant la corniche supérieure de 36 statues symbolisant les grandes villes de France.</p>
+    <p>On 9 October 1823, Louis XVIII decided to immediately resume and complete the Arc de Triomphe, but with a new dedication celebrating the Army of the Pyrenees. Work gradually resumed, under the direction of Louis-Robert Goust, now associated with the architect Jean-Nicolas Huyot. Together, they reworked Chalgrin\'s plans, in particular adding columns to the façade. Charles X succeeded Louis XVIII in 1824 and continued the work in accordance with the wishes of his predecessor..</p>
+  </div>
+  <div class="center-text">
+    <p>In 1825, Huyot was dismissed for his proposal to add columns to the façade, but he was reinstated in 1826. Charles X insisted that Chalgrin \'s plans be respected. In 1828, a commission of architects defined the ornamentation to be created, including caissons with rosettes and sculpted high reliefs on the façades. Chérence stone was chosen for the sculptures on the entablature. In 1829, the monument reached the architrave of the entablature, and a marble plaque with the new dedication to the Armée des Pyrénées was installed. In 1830, Goust retired, leaving Huyot alone on the site. Huyot proposed a project to decorate the upper cornice with 36 statues symbolising France\'s major cities.</p>
   </div>
   <div class="center-text">
     <p></p>
   </div>
   <div class="rectangle-orange">
-    <p>Vous souhaitez visiter Paris mais vous ne savez pas par où commencer ? </p>
-    <p> Découvrez nos parcours prédéfinis selon votre temps et vos envies !</p>
+    <p>Would you like to visit Paris but don\'t know where to start?</p>
+    <p>Discover our predefined itineraries to suit your time and desires !</p>
     <div class="texterectangle">
-      <a href="Parcourir.php">Parcourez ></a>
+      <a href="Parcourir.php">Visit ></a>
     </div>
   </div>
   <div class="texte-au-dessus-de-ligne">
-    <p>L'inauguration de L'Arc de triomphe</p>
-    <div class="ligne3">
-    </div>
+    <p>LInauguration of the Arc de Triomphe</p>
+    <div class="ligne3"></div>
   </div>
-  <div class="Champs-Info3">
-  </div>
-
+  <div class="Champs-Info3"></div>
   <div class="sousTitre">
-    <h2>Une inauguration discrète</h2>
-    </div>
-    <div class="center-text2">
-    <p>Le 29 juillet 1836 marque l'inauguration de l'Arc de Triomphe après trois décennies de travaux. Les listes de noms gravés sur les piédroits sont dévoilées lors de la cérémonie, mais la grande fête prévue est annulée par crainte d'attentat contre le roi Louis-Philippe. Seulement onze personnes assistent à l'événement, dont le président du Conseil des ministres Adolphe Thiers et le ministre des Finances Antoine Maurice Appolinaire Argout.</p>
-    </div>
-    <div class="center-text2">
-    <p>À la nuit tombée, une foule se rassemble devant l'Arc de Triomphe illuminé de 700 becs de gaz. Cependant, des controverses surgissent concernant les noms inscrits sur les piliers, entraînant des protestations. En réponse, le gouvernement et l'architecte promettent d'étudier toutes les demandes. L'architecte Blouet ajoute rapidement 128 noms de généraux et 172 batailles oubliés. Dans son ouvrage de 1837 intitulé "Les Voix Intérieures", Victor Hugo exprimera son chagrin de ne pas voir le nom de son père, Jospeh-Léopold-Sigisbert Hugo, nommé général par Louis XVIII en 1814, figurant sur l'Arc.</p>
-    </div>
-    <div class="center-text2">
-    <p>Des ajouts ponctuels aux noms gravés sur l'Arc de Triomphe se poursuivent jusqu'en 1895, reflétant ainsi une histoire complexe et dynamique liée à ce monument emblématique de Paris.</p>
+    <h2>A discreet inauguration</h2>
   </div>
-    <br>
+  <div class="center-text2">
+    <p>The Arc de Triomphe was inaugurated on 29 July 1836 after three decades of construction work. The lists of names engraved on the pedestals were unveiled at the ceremony, but the planned grand celebration was cancelled for fear of an assassination attempt on King Louis-Philippe. Only eleven people attended the event, including the President of the Council of Ministers Adolphe Thiers and the Minister of Finance Antoine Maurice Appolinaire Argout.</p>
+  </div>
+  <div class="center-text2">
+    <p>As night fell, crowds gathered in front of the Arc de Triomphe, illuminated by 700 gaslights. However, controversy arose over the names inscribed on the pillars, leading to protests. In response, the government and the architect promised to consider all requests. Architect Blouet quickly added 128 names of generals and 172 forgotten battles. In his 1837 book "Les Voix Intérieures", Victor Hugo expressed his chagrin at not seeing the name of his father, Jospeh-Léopold-Sigisbert Hugo, appointed general by Louis XVIII in 1814, appear on the Arc.</p>
+  </div>
+  <div class="center-text2">
+    <p> Occasional additions to the names engraved on the Arc de Triomphe continued until 1895, reflecting the complex and dynamic history of this iconic Paris monument.</p>
+  </div>
+  <br>
   <br>
   <div class="texte-au-dessus-de-ligne">
-    <div class="ligne3">
-    </div>
-  </div>  
+    <div class="ligne3"></div>
+  </div>';
+
+      }
+  ?>
+
+
   <!-- Footer -->
-  
   <?php
   include("footer.inc.php");
   ?>
+
 <script src="script.js"></script>
 </body>
 </html>
