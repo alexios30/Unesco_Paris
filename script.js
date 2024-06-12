0ddde154e1d65d9956c7f895379531ce1aba6a6d
Jeremyea3001 <151014662+Jeremyea3001@users.noreply.github.com>
2024-06-12 23:18:02 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function myFunction(elem) {
-  var x = document.getElementById(elem);
-  if (x.style.display === "none") {
-    x.style.display = "flex";
-  } else {
-    x.style.display = "none";
-  }
-} 
