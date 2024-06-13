2adf8f7781b3803f276981a3584fe297f24dc6bc
alexios30 <147252629+alexios30@users.noreply.github.com>
2024-06-13 16:29:32 +0200

    ajout de l'echo (manque la trad)

--- about_us.php
+++ about_us.php
@@ -12,85 +12,91 @@ include("checklang.inc.php");
 </head>
 <body>
     <?php
-    include("navbar.inc.php");
-    ?>
-    <header>
-        <nav>
-            <ul>
-                <li><a href="index.php?lang=<?php echo "$lang"; ?>">Accueil</a></li>
-                >
-                <li><a id="this" href="">Qui sommes-nous ?</a></li>
-            </ul>
-        </nav>
-    </header>
-    <main>
-        <h1>QUI SOMMES-NOUS ?</h1>
-        <hr class="main-line">
-        <p>Nous sommes une équipe de 5 étudiants en première année de BUT Informatique à l'IUT de Marne-La-Vallée. Ce projet a été réalisé dans le cadre de la médiation culturelle et numérique en partenariat avec l'UNESCO, dans le but de promouvoir un patrimoine culturel sur un site Internet.</p>
-        <hr class="separator-line">
-        <section class="team">
-            <article class="member">
-                <div class="top">
-                    <h2>Emilie XU</h2>
-                    <p>Chef(fe) de projet</p>
-                    <div class="photo"></div>
-                </div>
-                <ul class="links">
-                    <li><a href="#">LinkedIn</a></li>
-                    <li><a href="#">Portfolio</a></li>
-                    <li><a href="#">CV</a></li>
-                </ul>
-            </article>
-            <article class="member">
-                <div class="top">
-                    <h2>Alexis CHAMPAULT</h2>
-                    <p>Rôle</p>
-                    <div class="photo"></div>
-                </div>
-                <ul class="links">
-                    <li><a href="#">LinkedIn</a></li>
-                    <li><a href="#">Portfolio</a></li>
-                    <li><a href="#">CV</a></li>
-                </ul>
-            </article>
-            <article class="member">
-                <div class="top">
-                    <h2>Romain LONCIN</h2>
-                    <p>Rôle</p>
-                    <div class="photo"></div>
-                </div>
-                <ul class="links">
-                    <li><a href="#">LinkedIn</a></li>
-                    <li><a href="#">Portfolio</a></li>
-                    <li><a href="#">CV</a></li>
-                </ul>
-            </article>
-            <article class="member">
-                <div class="top">
-                    <h2>Jérémy ZHENG</h2>
-                    <p>Rôle</p>
-                    <div class="photo"></div>
-                </div>
-                <ul class="links">
-                    <li><a href="#">LinkedIn</a></li>
-                    <li><a href="#">Portfolio</a></li>
-                    <li><a href="#">CV</a></li>
-                </ul>
-            </article>
-            <article class="member">
-                <div class="top">
-                    <h2>Andrei ACHIRECESEI</h2>
-                    <p>Rôle</p>
-                    <div class="photo"></div>
-                </div>
-                <ul class="links">
-                    <li><a href="#">LinkedIn</a></li>
-                    <li><a href="#">Portfolio</a></li>
-                    <li><a href="#">CV</a></li>
-                </ul>
-            </article>
-        </section>
-    </main>
+        include("navbar.inc.php");
+        switch ($lang) {
+            case "fr":
+            echo '
+            <header>
+                <nav>
+                    <ul>
+                        <li><a href="index.php?lang=' . $lang . '">Accueil</a></li>
+                        >
+                        <li><a id="this" href="">Qui sommes-nous ?</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                <h1>QUI SOMMES-NOUS ?</h1>
+                <hr class="main-line">
+                <p>Nous sommes une équipe de 5 étudiants en première année de BUT Informatique à l\'IUT de Marne-La-Vallée. Ce projet a été réalisé dans le cadre de la médiation culturelle et numérique en partenariat avec l\'UNESCO, dans le but de promouvoir un patrimoine culturel sur un site Internet.</p>
+                <hr class="separator-line">
+                <section class="team">
+                    <article class="member">
+                        <div class="top">
+                            <h2>Emilie XU</h2>
+                            <p>Chef(fe) de projet</p>
+                            <div class="photo"></div>
+                        </div>
+                        <ul class="links">
+                            <li><a href="#">LinkedIn</a></li>
+                            <li><a href="#">Portfolio</a></li>
+                            <li><a href="#">CV</a></li>
+                        </ul>
+                    </article>
+                    <article class="member">
+                        <div class="top">
+                            <h2>Alexis CHAMPAULT</h2>
+                            <p>Rôle</p>
+                            <div class="photo"></div>
+                        </div>
+                        <ul class="links">
+                            <li><a href="#">LinkedIn</a></li>
+                            <li><a href="#">Portfolio</a></li>
+                            <li><a href="#">CV</a></li>
+                        </ul>
+                    </article>
+                    <article class="member">
+                        <div class="top">
+                            <h2>Romain LONCIN</h2>
+                            <p>Rôle</p>
+                            <div class="photo"></div>
+                        </div>
+                        <ul class="links">
+                            <li><a href="#">LinkedIn</a></li>
+                            <li><a href="#">Portfolio</a></li>
+                            <li><a href="#">CV</a></li>
+                        </ul>
+                    </article>
+                    <article class="member">
+                        <div class="top">
+                            <h2>Jérémy ZHENG</h2>
+                            <p>Rôle</p>
+                            <div class="photo"></div>
+                        </div>
+                        <ul class="links">
+                            <li><a href="#">LinkedIn</a></li>
+                            <li><a href="#">Portfolio</a></li>
+                            <li><a href="#">CV</a></li>
+                        </ul>
+                    </article>
+                    <article class="member">
+                        <div class="top">
+                            <h2>Andrei ACHIRECESEI</h2>
+                            <p>Rôle</p>
+                            <div class="photo"></div>
+                        </div>
+                        <ul class="links">
+                            <li><a href="#">LinkedIn</a></li>
+                            <li><a href="#">Portfolio</a></li>
+                            <li><a href="#">CV</a></li>
+                        </ul>
+                    </article>
+                </section>
+            </main>';
+            break;
+        }
+        ?>
+
     <?php
     include("footer.inc.php");
     ?>
