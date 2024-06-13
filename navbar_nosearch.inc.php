5b592882f3c05285127717561ec7b8eaebc1a7e0
alexios30 <147252629+alexios30@users.noreply.github.com>
2024-06-13 18:17:23 +0200

    ajustement des redirections

--- navbar_nosearch.inc.php
+++ navbar_nosearch.inc.php
@@ -1,43 +1,102 @@
 <?php
-echo '
+if (! isset($lang)) {
+    $lang = "fr";
+}
+
+$adresse = $_SERVER['PHP_SELF'];
+$composants_adresse = explode('/', $adresse);
+$fichier_avec_ext = end($composants_adresse);
+$fichier_sep = explode('.', $fichier_avec_ext);
+$nom_fichier = $fichier_sep[0];
+
+$langs = [
+    'fr' => ['Rechercher...', 'Participer', 'Parcourir', 'Découvrir', 'FR', 'https://i.imgur.com/moe4dvF.png'],
+    'en' => ['Research...', 'Participate', 'Route', 'Discover', 'EN', 'https://i.imgur.com/ZUd06Ts.png'],
+    'es' => ['Buscar...', 'Participar', 'Explorar', 'Descubrir', 'ES', 'https://i.imgur.com/6MrXRwG.png']
+];
+
+$current_lang = $langs[$lang];
+$other_langs = array_diff_key($langs, [$lang => '']);
+
+echo "
 <!-- Barre de navigation -->
-  
-<div class="navigbar">
-  <div class="navig">
-    
-    <!-- Logo MCN -->
-    
-    <a class="logo" href="index.php"><img id="LogoMCN" src="https://i.imgur.com/FYHl6nN.png"></a>
-    
-    <!-- Menu des langues -->
-    
-    <button class="menul" onClick="myFunction('."'".'langues'."'".')">
-      <img src="https://i.imgur.com/moe4dvF.png" class="langue"/>&nbsp;FR
-    </button>
-    <div id="langues" style="display: none;">
-      <a href=""><div class="sousl">
-        <img src="https://i.imgur.com/ZUd06Ts.png" class="langue"/>&nbsp;EN
-      </div></a>
-      <a href=""><div class="sousl">
-        <img src="https://i.imgur.com/6MrXRwG.png" class="langue"/>&nbsp;ES
-      </div></a>
+<div class='navigbar'>
+  <div class='navig'>
+    <div id='gauche'>
+      <!-- Hamburger menu -->
+      <button onclick='myFunction(\"menu\")' class='rectangles'>
+        <img id='hambmenu' src='https://i.imgur.com/MCTSRpB.png'>
+      </button>
+      <!-- Logo MCN -->
+      <a class='logo' href='index.php?lang=" . htmlspecialchars($lang) . "'><img id='LogoMCN' src='https://i.imgur.com/FYHl6nN.png'></a>
+    </div>
+        
+    <!-- Barre de recherche -->
+    <div id='droite'>
+      <form method='post' action='" . htmlspecialchars($nom_fichier) . ".php?lang=" . htmlspecialchars($lang) . "' id='researchBar'>
+        <input class='recherche' type='text' placeholder='" . htmlspecialchars($current_lang[0]) . "'>
+        <input class='loupe' type='submit' value=''>
+      </form>
+      <button class='loupe' onClick='showResearchBar()' id='smallerResearch'></button>
+      <!-- Menu des langues -->    
+      <div id='container'>
+        <button class='menul' onClick='myFunctionlang(\"langues\")'>
+          <img src='" . htmlspecialchars($current_lang[5]) . "' class='langue'/>&nbsp;" . htmlspecialchars($current_lang[4]) . "
+        </button>
+        <div id='langues' style='display: none;'>";
+foreach ($other_langs as $key => $value) {
+    echo "
+          <a href='" . htmlspecialchars($nom_fichier) . ".php?lang=" . htmlspecialchars($key) . "'>
+            <div class='sousl'>
+              <img src='" . htmlspecialchars($value[5]) . "' class='langue'/>&nbsp;" . htmlspecialchars($value[4]) . "
+            </div>
+          </a>";
+}
+echo "
+        </div>
+      </div>
     </div>
   </div>
-  
   <!-- Reste du menu hamburger -->
-  
-  <div id="menu">
-    <div class="sous">
-      <a href="participer_1.php">Participer</a>
+  <div id='menu' style='display: none;'>
+    <div class='sous'>
+      <a href='participer_1.php?lang=" . htmlspecialchars($lang) . "'>" . htmlspecialchars($current_lang[1]) . "</a>
     </div>
-    <div class="sous">
-      <a href="Parcourir.php">Parcourir</a>
+    <div class='sous'>
+      <a href='Parcourir.php?lang=" . htmlspecialchars($lang) . "'>" . htmlspecialchars($current_lang[2]) . "</a>
     </div>
-    <div class="sous">
-      <a href="decouvrir.php">D&eacute;couvrir</a>
+    <div class='sous'>
+      <a href='decouvrir.php?lang=" . htmlspecialchars($lang) . "'>" . htmlspecialchars($current_lang[3]) . "</a>
     </div>
   </div>
 </div>
-</div>
-';
+
+<script>
+function myFunction(elem) {
+  var x = document.getElementById(elem);
+  if (x.style.display === \"none\") {
+    x.style.display = \"flex\";
+  } else {
+    x.style.display = \"none\";
+  }
+} 
+
+function myFunctionlang(elem) {
+  var x = document.getElementById(elem);
+  var menu = document.getElementById('container');
+  if (x.style.display === 'none') {
+    x.style.display = 'flex';
+    menu.style.transform = 'translateY(61px)';
+  } else {
+    x.style.display = 'none';
+    menu.style.transform = 'translateY(0px)';
+  }
+}
+
+function showResearchBar() {
+  var x = document.getElementById('researchBar');
+  x.style.display = 'flex';
+}
+</script>
+";
 ?>
