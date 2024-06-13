7b38ff8ae03ad35c697699d6e5792570f35daad1
Jeremyea3001 <151014662+Jeremyea3001@users.noreply.github.com>
2024-06-13 20:34:49 +0200

    page parcourir + responsive

---
+++ parcourir.php
@@ -0,0 +1,137 @@
+<?php
+include("checklang.inc.php");
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parcourir</title>
+    <link rel="stylesheet" href="style_parcourir.css">
+</head>
+<body>
+    <?php
+    include("navbar.inc.php");
+    ?>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php?lang=<?php echo "$lang"; ?>">Accueil</a></li>
+                >
+                <li><a id="this" href="">Parcourir</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>Parcourir</h1>
+        <hr class="main-line">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse 
+            luctus laoreet dolor quis imperdiet. Proin nec massa massa. Aliquam 
+            finibus nisi quis neque pharetra condimentum. Donec malesuada 
+            scelerisque turpis.
+        </p>
+        <hr class="separator-line">
+        <section class="main-section">
+            <article class="parcours">
+                <div class="left-side">
+                    <img id="img1" src="https://i.imgur.com/aT2fyJh.jpg" alt="image 1">
+                    <img src="https://i.imgur.com/aT2fyJh.jpg" alt="image 2">
+                    <img src="https://i.imgur.com/aT2fyJh.jpg" alt="image 3">
+                </div>
+                <div class="right-side">
+                    <h2>Nom du parcours</h2>
+                    <nav>
+                        <ul>
+                            <li>tag</li>
+                            <li>tag</li>
+                            <li>tag</li>
+                        </ul>
+                    </nav>
+                    <p>
+                        Dui metus nec lacus. Ut ac malesuada dolor, in sagittis neque. 
+                        Suspendisse sollicitudin urna libero, in ullamcorper nisl elementum et.
+
+
+                        Sed vehicula accumsan quam, auctor pharetra neque varius eget. 
+                        Suspendisse maximus, tortor vitae sagittis dictum, nulla libero 
+                        facilisis nisl, vel accumsan nunc leo vitae nulla. Quisque leo ante, 
+                        aliquam quis tellus et, maximus pulvinar velit. Integer aliquet accumsan
+                        turpis a faucibus. Curabitur bibendum nibh vel lectus euismod, ac 
+                        finibus magna facilisis. Donec posuere rutrum ante, ac vehicula metus. 
+                        Suspendisse potenti. Aliquam in ipsum viverra, feugiat metus ac, commodo
+                        magna. Curabitur molestie feugiat sem sit amet porta. Donec ex ex, 
+                        tristique nec quam tincidunt, pretium bibendum ante. Quisque ornare non 
+                        nunc ac imperdiet. Aenean vel posuere dolor, id maximus est.
+
+
+                        Suspendisse semper urna sed quam viverra, eget interdum velit mollis. 
+                        Sed laoreet ante non ornare fermentum. Donec vehicula pulvinar nibh, et 
+                        aliquet erat egestas nec. Aliquam arcu urna, interdum vitae lacus eget, 
+                        sodales pulvinar felis. Cras libero lacus, ultricies id augue eu, 
+                        scelerisque tempus felis. Fusce sit amet ligula nunc. Aenean in pharetra
+                        sapien. Donec dignissim justo eget tincidunt pretium. Nulla ut sagittis
+                        odio. Aliquam accumsan, risus a placerat eleifend, erat leo dapibus 
+                        nunc, non aliquam magna massa et lacus. Maecenas arcu metus, maximus et 
+                        nisi sit amet, maximus eleifend orci. Sed dictum massa sed urna 
+                        suscipit, eu dictum nunc convallis. Nulla facilisi. Sed lobortis risus 
+                        sit amet sem posuere auctor. Quisque lacinia nibh a ipsum venenatis, at 
+                        volutpat leo ultricies.
+                    </p>
+                </div>
+            </article>
+            <article class="parcours">
+                <div class="left-side">
+                    <img id="img1" src="https://i.imgur.com/aT2fyJh.jpg" alt="image 1">
+                    <img src="https://i.imgur.com/aT2fyJh.jpg" alt="image 2">
+                    <img src="https://i.imgur.com/aT2fyJh.jpg" alt="image 3">
+                </div>
+                <div class="right-side">
+                    <h2>Nom du parcours</h2>
+                    <nav>
+                        <ul>
+                            <li>tag</li>
+                            <li>tag</li>
+                            <li>tag</li>
+                        </ul>
+                    </nav>
+                    <p>
+                        Dui metus nec lacus. Ut ac malesuada dolor, in sagittis neque. 
+                        Suspendisse sollicitudin urna libero, in ullamcorper nisl elementum et.
+
+
+                        Sed vehicula accumsan quam, auctor pharetra neque varius eget. 
+                        Suspendisse maximus, tortor vitae sagittis dictum, nulla libero 
+                        facilisis nisl, vel accumsan nunc leo vitae nulla. Quisque leo ante, 
+                        aliquam quis tellus et, maximus pulvinar velit. Integer aliquet accumsan
+                        turpis a faucibus. Curabitur bibendum nibh vel lectus euismod, ac 
+                        finibus magna facilisis. Donec posuere rutrum ante, ac vehicula metus. 
+                        Suspendisse potenti. Aliquam in ipsum viverra, feugiat metus ac, commodo
+                        magna. Curabitur molestie feugiat sem sit amet porta. Donec ex ex, 
+                        tristique nec quam tincidunt, pretium bibendum ante. Quisque ornare non 
+                        nunc ac imperdiet. Aenean vel posuere dolor, id maximus est.
+
+
+                        Suspendisse semper urna sed quam viverra, eget interdum velit mollis. 
+                        Sed laoreet ante non ornare fermentum. Donec vehicula pulvinar nibh, et 
+                        aliquet erat egestas nec. Aliquam arcu urna, interdum vitae lacus eget, 
+                        sodales pulvinar felis. Cras libero lacus, ultricies id augue eu, 
+                        scelerisque tempus felis. Fusce sit amet ligula nunc. Aenean in pharetra
+                        sapien. Donec dignissim justo eget tincidunt pretium. Nulla ut sagittis
+                        odio. Aliquam accumsan, risus a placerat eleifend, erat leo dapibus 
+                        nunc, non aliquam magna massa et lacus. Maecenas arcu metus, maximus et 
+                        nisi sit amet, maximus eleifend orci. Sed dictum massa sed urna 
+                        suscipit, eu dictum nunc convallis. Nulla facilisi. Sed lobortis risus 
+                        sit amet sem posuere auctor. Quisque lacinia nibh a ipsum venenatis, at 
+                        volutpat leo ultricies.
+                    </p>
+                </div>
+            </article>
+        </section>
+    </main>
+    <?php
+    include("footer.inc.php");
+    ?>
+</body>
+</html>
